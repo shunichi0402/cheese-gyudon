029385ecd366a11614c69184db68e2cfdc26e7b8
Shunichi0402 <shunichi_nikoniko@yahoo.co.jp>
2021-11-24 21:07:28 +0900

    add studnetを追加

--- pages/dev/database/main.js
+++ pages/dev/database/main.js
@@ -1,5 +1,30 @@
 const database = firebase.database();
 
+async function addStudent(name, number, studentClass){
+    if(!name || !number || !studentClass){
+        return
+    }
+
+    const classData = await database.ref(`class/${studentClass}`).get();
+    const parseClassData = await classData.val();
+
+    console.log(parseClassData);
+
+    if (parseClassData == null){
+        console.log('err : classIDが不正な値です。');
+        return
+    }
+
+    const result = await database.ref('student').push(
+        {
+            name,
+            number
+        }
+    );
+
+    console.log(`success : ${result}に追加されました。`);
+}
+
 function addClass(name){
     if(name){
         database.ref('class').get().then(data => {
@@ -19,6 +44,7 @@ function addClass(name){
                     }
                 ).then(id => {
                     console.log(`success : ${id}に追加されました。`);
+                    relocadClass();
                 });
             } else {
                 console.log('err : この名前は既に使用されています。');
@@ -36,11 +62,26 @@ function relocadClass(){
         for (const [key, value] of Object.entries(parsedData)) {
             innerHTML += `<option value="${key}">${value.name}</option>`;
         }
-        document.getElementById('input-student-class').innerHTML = innerHTML;
+        document.getElementById('student-class').innerHTML = innerHTML;
     });
 }
 
 document.getElementById('add-class-button').addEventListener('click', () => {
     addClass(document.getElementById('class-name').value);
-})
+    document.getElementById('class-name').value = '';
+});
+
+document.getElementById('add-student-button').addEventListener('click', () => {
+    const name = document.getElementById('student-name');
+    const number = document.getElementById('student-number');
+    const studentClass = document.getElementById('student-class');
+    console.log(name);
+    console.log(number);
+    console.log(studentClass);
+    addStudent(name.value, number.value, studentClass.value);
+    name.value = ''; number.value = '';
+});
+
+
+relocadClass();
 
